0f3431d2cc5bfe5b1178804c8813d233d2da13ad
igoryudnikov <igoryudnikov@gmail.com>
2017-06-12 10:54:39 +0300

    Just working. Just awesome.

--- src/main/scala/ru/yudnikov/bintree/Empty.scala
+++ src/main/scala/ru/yudnikov/bintree/Empty.scala
@@ -5,25 +5,25 @@ package ru.yudnikov.bintree
   */
 class Empty[T](val compare: (T, T) => Boolean) extends Node[T] {
 
-  override def toString: String = "."
-
-  override def include(x: T): NonEmpty[T] = new NonEmpty(compare)(x)
+  override def collect: List[T] = List()
 
   override def contains(x: T): Boolean = false
 
+  override def filter(f: (T) => Boolean): Node[T] = this
+
+  override def foreach(f: (T) => Unit): Unit = {}
+  
+  override def include(x: T): NonEmpty[T] = new NonEmpty(compare)(x)
+  
+  override def include(x: T*): NonEmpty[T] = new NonEmpty[T](compare)(x.head).include(x.tail: _*)
+  
   override def merge(other: Node[T]): Node[T] = other
   
   override def remove(x: T): Node[T] = this
   
-  override def filter(f: (T) => Boolean): Node[T] = this
-  
-  override def foreach(f: (T) => Unit): Unit = {}
-  
   override def sortBy(f: (T, T) => Boolean): Node[T] = this
   
-  override def withGreaterFunction(f: (T, T) => Boolean): Node[T] = new Empty(compare)
+  override def toString: String = "."
   
-  override def collect: List[T] = List()
-  
-  override def include(x: T*): NonEmpty[T] = new NonEmpty[T](compare)(x.head).include(x.tail: _*)
+  override def withOrderFunction(f: (T, T) => Boolean): Node[T] = new Empty(compare)
 }
--- src/main/scala/ru/yudnikov/bintree/Node.scala
+++ src/main/scala/ru/yudnikov/bintree/Node.scala
@@ -11,21 +11,21 @@ abstract class Node[T] {
   
   def collect: List[T]
   
-  def withGreaterFunction(f: CompareFunction): Node[T]
+  def contains(x: T):  Boolean
+
+  def filter(p: (T) => Boolean): Node[T]
+  
+  def foreach(f: (T) => Unit): Unit
 
   def include(x: T): NonEmpty[T]
-  
+
   def include(x: T*): NonEmpty[T]
-
-  def contains(x: T): Boolean
-
+  
   def merge(other: Node[T]): Node[T]
   
   def remove(x: T): Node[T]
   
-  def filter(p: (T) => Boolean): Node[T]
-  
-  def foreach(f: (T) => Unit): Unit
-  
   def sortBy(f: CompareFunction): Node[T]
+  
+  def withOrderFunction(f: CompareFunction): Node[T]
 }
